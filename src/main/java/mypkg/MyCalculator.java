2dc3b5b5cc551d667f5209fa56c57a74eb813f4e
dhruvag1978 <dhruvagarwal1453@gmail.com>
2023-08-30 15:42:14 +0530

    Second Build

--- src/main/java/mypkg/MyCalculator.java
+++ src/main/java/mypkg/MyCalculator.java
@@ -11,12 +11,21 @@ public class MyCalculator {
 	{
 		return(a-b);
 	}
+	public int div(int a, int b)
+	{
+		return(a/b);
+	}
+	public int mul(int a, int b)
+	{
+		return(a*b);
+	}
 	public static void main(String[] args) {
 		
 		MyCalculator calc = new MyCalculator ();
 		System.out.println("Sum is "+calc.sum(20, 10));
-		System.out.println("Diff is "+calc.diff(20, 10));
-		
+		System.out.println("Difference is "+calc.diff(20, 10));
+		System.out.println("Division is "+calc.div(20, 10));
+		System.out.println("Multiplication is "+calc.mul(20, 10));
 	
 	}
 
